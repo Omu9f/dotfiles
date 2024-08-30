f2930cd01c7fa0486b040d8f87ae812309380f4c
MosesDVarghese <accydent@gmail.com>
2024-08-30 20:21:37 +0400

    add nvim configs

---
+++ nvim/lua/config/lazy.lua
@@ -0,0 +1,50 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  -- bootstrap lazy.nvim
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+    -- add LazyVim and import its plugins
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import any extras modules here
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.lang.json" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- import/override with your plugins
+    { import = "plugins" },
+
+    -- Add tokyonight.nvim plugin
+    { "folke/tokyonight.nvim" },
+  },
+  defaults = {
+    lazy = false, -- do not lazy-load custom plugins by default
+    version = false, -- always use the latest git commit
+  },
+  install = { colorscheme = { "tokyonight" } }, -- ensure tokyonight is installed
+  checker = { enabled = true }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
+
+-- Set colorscheme to tokyo night night
+vim.cmd([[colorscheme tokyonight-night]])
