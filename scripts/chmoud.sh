cea64435058e802de61b012f637040610535231e
MosesDVarghese <accydent@gmail.com>
2024-12-06 22:57:04 +0400

    update btop

---
+++ scripts/chmoud.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# script to update everything together
+
+# Ensure the scripts are executable
+chmod +x ./update_nvim.sh
+chmod +x ./update_tmux.sh
+chmod +x ./update_kitty.sh
+chmod +x ./update_hypr.sh
+chmod +x ./update_waybar.sh
+chmod +x ./update_wofi.sh
+chmod +x ./update_btop.sh
--- scripts/update_all.sh
+++ scripts/update_all.sh
@@ -2,15 +2,6 @@
 
 # script to update everything together
 
-# Ensure the scripts are executable
-chmod +x ./update_nvim.sh
-chmod +x ./update_tmux.sh
-chmod +x ./update_kitty.sh
-chmod +x ./update_hypr.sh
-chmod +x ./update_waybar.sh
-chmod +x ./update_wofi.sh
-chmod +x ./update_btop.sh
-
 echo "Running update_nvim.sh..."
 ./update_nvim.sh
 
