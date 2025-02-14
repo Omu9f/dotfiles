15ad9030a9ff1564dfe8a85cd787330447178bd0
MosesDVarghese <accydent@gmail.com>
2025-02-14 16:15:08 +0400

    the best hyprwm remap ever

--- terminal/nvim/lua/src/plugins/a_theme.lua
+++ terminal/nvim/lua/src/plugins/a_theme.lua
@@ -1,27 +1,5 @@
 -- theme for aesthetics
 return {
-  -- {
-  --   dir = "~/codespace/superdev/memento.nvim",
-  --   priority = 1000,
-  --   config = function()
-  --     require("memento").setup({
-  --       style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-  --       transparent = true, -- Enable this to disable setting the background color
-  --       terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-  --       styles = {
-  --         -- Style to be applied to different syntax groups
-  --         -- Value is any valid attr-list value for `:help nvim_set_hl`
-  --         comments = { italic = false },
-  --         keywords = { italic = false },
-  --         -- Background styles. Can be "dark", "transparent" or "normal"
-  --         sidebars = "transparent", -- style for sidebars, see below
-  --         floats = "transparent", -- style for floating windows
-  --       },
-  --     })
-  --     -- set the colorscheme
-  --     vim.cmd.colorscheme("memento")
-  --   end,
-  -- },
   {
     dir = "~/codespace/superdev/sumtheme.nvim",
     priority = 1000,
@@ -83,8 +61,69 @@ return {
     config = function()
       vim.g.gruvbox_material_transparent_background = "0"
       vim.g.gruvbox_material_enable_italic = false
+      vim.g.gruvbox_material_set_background = "light"
       -- set the colorscheme
       -- vim.cmd.colorscheme("gruvbox-material")
+    end,
+  },
+  {
+    "rose-pine/neovim",
+    priority = 1000,
+    config = function()
+      require("rose-pine").setup({
+        variant = "auto", -- auto, main, moon, or dawn
+        dark_variant = "main", -- main, moon, or dawn
+        dim_inactive_windows = false,
+        extend_background_behind_borders = true,
+
+        enable = {
+          terminal = true,
+          legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+          migrations = true, -- Handle deprecated options automatically
+        },
+
+        styles = {
+          bold = true,
+          italic = false,
+          transparency = false,
+        },
+
+        groups = {
+          border = "muted",
+          link = "iris",
+          panel = "surface",
+
+          error = "love",
+          hint = "iris",
+          info = "foam",
+          note = "pine",
+          todo = "rose",
+          warn = "gold",
+
+          git_add = "foam",
+          git_change = "rose",
+          git_delete = "love",
+          git_dirty = "rose",
+          git_ignore = "muted",
+          git_merge = "iris",
+          git_rename = "pine",
+          git_stage = "iris",
+          git_text = "rose",
+          git_untracked = "subtle",
+
+          h1 = "iris",
+          h2 = "foam",
+          h3 = "rose",
+          h4 = "gold",
+          h5 = "pine",
+          h6 = "foam",
+        },
+      })
+
+      -- vim.cmd("colorscheme rose-pine")
+      -- vim.cmd("colorscheme rose-pine-main")
+      -- vim.cmd("colorscheme rose-pine-moon")
+      -- vim.cmd("colorscheme rose-pine-dawn")
     end,
   },
 }
